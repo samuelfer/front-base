beec96d3cc8006efd59a269d91e5aea530e34079
sfsantos <sfsantos@tce.pb.gov.br>
2022-01-22 12:45:46 -0300

    Salvando dados em uma api de testes

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,3 +1,5 @@
+import { PostApi } from './../post-api';
+import { ClienteService } from './../../services/cliente.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -11,14 +13,24 @@ import { Cliente } from '../cliente';
 export class ClientesFormComponent implements OnInit {
 
   public cliente: Cliente = new Cliente(0, '', '', '');
+  public postApi: PostApi = new PostApi(1, '', '');
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private clienteService: ClienteService
+  ) {}
 
   ngOnInit(): void {
   }
 
   onSubmit() {
-    console.log(this.cliente)
+    this.clienteService.salvar(this.postApi)
+      .subscribe(response => {
+        console.log(response)
+      },
+      errorResponse => {
+        console.log(errorResponse)
+      });
   }
 
   voltar(): void {
--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { Cliente } from '../clientes/cliente';
+import { PostApi } from '../clientes/post-api';
 import { UsuarioApi } from '../clientes/usuario-api';
 
 @Injectable({
@@ -12,13 +13,13 @@ export class ClienteService {
 
   constructor(private http: HttpClient) { }
 
-  private baseUrl = 'https://jsonplaceholder.typicode.com/users';
+  private baseUrl = 'https://jsonplaceholder.typicode.com';
 
   getClientes(): Observable<UsuarioApi[]> {
-    return this.http.get<UsuarioApi[]>(this.baseUrl);
+    return this.http.get<UsuarioApi[]>(`${this.baseUrl}/users`);
   }
 
-  salvar(cliente: Cliente): Observable<Cliente> {
-    return this.http.post<Cliente>(this.baseUrl, cliente);
+  salvar(postApi: PostApi): Observable<PostApi> {
+    return this.http.post<PostApi>(`${this.baseUrl}/posts`, postApi);
   }
 }
